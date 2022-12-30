c12807e199dfdddd88a19ebb8ec5d8e8d06a7b17
Mina <romi2662gmail.com>
2022-12-30 02:41:43 -0400

    1st complete exercise of the exam

---
+++ 1er_Exercise.py
@@ -0,0 +1,24 @@
+def exp2(t):
+    n = t**2
+    return n
+i = 0
+while i < 3:
+    N = input ('Ingresar un número de 4 digitos: ')
+    li = len(N)
+    if N.isnumeric() and li >= 4:
+        N = int(N); k = str(exp2(N)); lf = len (k); dif = lf - li
+        u1 = dif // 2; v1 = u1 + li
+        ale = k[u1:v1]
+        print ('0,' + ale)
+        i = 3
+    elif i < 2:
+        oportunities = 2 - i
+        i = i + 1
+        if oportunities == 1:
+            word = 'intento'
+        else:
+            word = 'intentos'
+        print ('Error en el dato ingresado. Vuelva a intentar.\nTiene {} {}.'.format(oportunities, word))
+    else:
+        print ('Error. Fin del programa.')
+        i = 3
