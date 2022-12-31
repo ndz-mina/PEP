20548ac9d94df255369938cd51f74d09052a4aa3
Mina <romi2662gmail.com>
2022-12-31 02:31:17 -0400

    Modifying the 'time' function

--- 2nd_Exercise.py
+++ 2nd_Exercise.py
@@ -2,9 +2,8 @@
 #Para distancias pertenecientes a los números enteros y aproximando 1 día a 24 hrs.
 t = 12; h_day = t; h_night = 24 - t
 def time(h_day, H, dr, Ld):
-    h_max = h_day * ((H - dr) / Ld)
-    h = h_max // 24
-    m_max = h_max % 24
+    h = (h_day * (H - dr)) // Ld
+    m_max = (h_day * (H - dr)) % Ld
     m = m_max // 60
     s = m_max % 60
     t = [h, m, s]
@@ -52,16 +51,22 @@ if H.isnumeric():
                     d = len(position) // 2
                     dr = position[-1]; dif = time(h_day, H, dr, Ld)
                     h = dif[0]; m = dif[1]; s = dif[2]
-                    if h != 0 and m != 0 and s != 0:
-                        print('El caracol saldrá del pozo en {} {} con {} {}, {} {} y {} {}.'.format(d, wd(d),h, wh(h), m, wm(m), s, ws(s)))
-                    elif h == 0 and m != 0 and s != 0:
-                        print('El caracol saldrá del pozo en {} {} con {} {} y {} {}.'.format(d, wd(d), m, wm(m), s, ws(s)))
-                    elif h != 0 and m == 0 and s != 0:
-                        print('El caracol saldrá del pozo en {} {} con {} {} y {} {}.'.format(d, wd(d),h, wh(h), s, ws(s)))
-                    elif h != 0 and m != 0 and s == 0:
-                        print('El caracol saldrá del pozo en {} {} con {} {} y {} {}.'.format(d, wd(d),h, wh(h), m, wm(m)))
-                    else:
+                    if h == 0 and m == 0 and s == 0:
                         print('El caracol saldrá del pozo en {} {}.'.format(d, wd(d)))
+                    elif h == 0 and m == 0:
+                        print('El caracol saldrá del pozo en {} {} con {} {}.'.format(d, wd(d), s, ws(s)))
+                    elif s == 0 and m == 0:
+                        print('El caracol saldrá del pozo en {} {} con {} {}.'.format(d, wd(d), h, wh(h)))
+                    elif h == 0 and s == 0:
+                        print('El caracol saldrá del pozo en {} {} con {} {}.'.format(d, wd(d), m, wm(m)))
+                    elif h == 0:
+                        print('El caracol saldrá del pozo en {} {} con {} {} y {} {}.'.format(d, wd(d), m, wm(m), s, ws(s)))
+                    elif m == 0:
+                        print('El caracol saldrá del pozo en {} {} con {} {} y {} {}.'.format(d, wd(d), h, wh(h), s, ws(s)))
+                    elif s == 0:
+                        print('El caracol saldrá del pozo en {} {} con {} {} y {} {}.'.format(d, wd(d), h, wh(h), m, wm(m)))
+                    else:
+                        print('El caracol saldrá del pozo en {} {} con {} {}, {} {} y {} {}.'.format(d, wd(d), h, wh(h), m, wm(m), s, ws(s)))
                 else:
                     print('El caracol nunca saldrá del pozo.')
             elif H == Ld:
