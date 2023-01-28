73201af29c2f1b7495335f979e9946a8bf53d083
Mina <romi2662gmail.com>
2023-01-28 15:57:11 -0400

    Optimized 1.1

--- 3th_Exercise.py
+++ 3th_Exercise.py
@@ -1,4 +1,8 @@
 #Código para generar una matriz NxM, con secuencia creciente en zig-zag horizontal
+
+N = input('Ingrese el número de fila: ')
+M = input('Ingrese el número de columna: ')
+
 def A(x, y):
     matrix = []
     n = 0; m = -1
@@ -17,14 +21,18 @@ def A(x, y):
             matrix.append(row)
     return matrix
 
-N = input('Ingrese el número de fila: ')
-if N.isnumeric():
-    M = input('Ingrese el número de columna: ')
-    if M.isnumeric():
-        N = int(N); M = int(M)
-        t = A(N, M)
-        print(t)
+if not N.isnumeric() or not M.isnumeric():
+    if N.isnumeric():
+        raise TypeError('El valor ingresado para columnas debe ser numérico.')
+    elif M.isnumeric():
+        raise TypeError('El valor ingresado para filas debe ser numérico.')
     else:
-        print('Error en el número de columnas.')
-else:
-    print('Error en el número de filas.')
+        TypeError('Los datos ingresados deben ser numéricos')
+    
+N = int(N); M = int(M)
+
+if N < 1 or M < 1:
+    raise ValueError('Los números ingresados deben ser mayor a cero.')
+
+t = A(N, M)
+print(t)
