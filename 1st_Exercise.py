eaaf5e8e4ff15bde62c3a278bffd6893ea288182
Mina <romi2662gmail.com>
2023-01-28 15:54:29 -0400

    Optimized 1.1

--- 1st_Exercise.py
+++ 1st_Exercise.py
@@ -1,27 +1,35 @@
 #Código para generar un número pseudoaleatoreo usando 
 #la técnica del cuadrado medio para dígitos mayores o iguales a cuatro.
+
 def exp2(t):
     n = t**2
     return n
-i = 0
-while i < 3:
+
+def word(x):
+    if x == 1:
+        c = 'intento'
+    else:
+        c = 'intentos'
+    return c
+
+i = 1
+while i < 4:
     N = input ('Ingresar un número de 4 digitos: ')
     li = len(N)
+
     if N.isnumeric() and li >= 4:
         N = int(N); k = str(exp2(N)); lf = len (k); dif = lf - li
         u1 = dif // 2
         v1 = u1 + li
         ale = k[u1:v1]
         print ('0,' + ale)
-        i = 3
-    elif i < 2:
-        oportunities = 2 - i
+        i = 4
+
+    elif i < 3:
+        oportunities = 3 - i
         i += 1
-        if oportunities == 1:
-            word = 'intento'
-        else:
-            word = 'intentos'
-        print ('Error en el dato ingresado. Vuelva a intentar.\nTiene {} {}.'.format(oportunities, word))
+        print ('Error en el dato ingresado. Vuelva a intentar.\nTienes {} {}.'.format(oportunities, word(oportunities)))
+    
     else:
         print ('Error. Fin del programa.')
-        i = 3
+        i = 4
