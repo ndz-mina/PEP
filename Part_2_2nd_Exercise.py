4c8fc3854228a7e2d56c204b7c11284b5a3a3731
Mina <romi2662gmail.com>
2023-01-28 16:03:17 -0400

    Optimized 1.1

--- Part_2_2nd_Exercise.py
+++ Part_2_2nd_Exercise.py
@@ -1,19 +1,28 @@
 #Cálculo aproximado del valor pi según una cantidad dada, por el usuario, de términos.
+
 M = input('Ingrese la cantidad de términos a sumar : ')
+
 def series(k):
     suma = 0
     for n in range(1, k + 1):
-        suma = suma + (((-1)**(n - 1))/(n**6))
+        suma += ((-1)**(n - 1)) / (n**6)
     return suma
+
 def word(x):
     if x == 1:
-        c = 'valor'
+        c = 'termino'
     else:
-        c = 'valores'
+        c = 'términos'
     return c
-if M.isnumeric():
-    M = int(M)
-    Aprox_pi = (30240 * series(M) / 31)**(1 / 6)
-    print('El valor de la aproximación de pi con {} {} es {}'.format(M, word(M), Aprox_pi))
-else:
-    print('Error en el valor ingresado.')
+
+if not M.isnumeric():
+    raise TypeError('El valor ingresado no es numérico.')
+
+M = int(M)
+
+if M < 0:
+    raise ValueError('El número debe ser mayor a cero.')
+
+Aprox_pi = (30240 * series(M) / 31)**(1 / 6)
+
+print('El valor de la aproximación de pi con {} {} es {}'.format(M, word(M), Aprox_pi))
