8f0d71bfe19f7d97d7cfcfcc1b2c4e00d6bfc858
Mina <romi2662gmail.com>
2023-02-23 01:18:04 -0400

    Modified

--- crypto.py
+++ crypto.py
@@ -1,30 +1,37 @@
 import random
 import string
+import modulo_Mina
 
-chars = " " + string.punctuation + string.digits + string.ascii_letters
-chars = list(chars)
-key = chars.copy()
+def encrypt(text):
 
-random.shuffle(key)
+    if modulo_Mina.test(text) == 'class "str"':
+        chars = " " + string.punctuation + string.digits + string.ascii_letters
+        chars = list(chars)
+        key = chars.copy(); random.shuffle(key)
+        cipher_text = ""
 
-#ENCRYPT
-plain_text = input("Ingrese el mensaje a encriptar : ")
-cipher_text = ""
+        for letter in text:
+            index = chars.index(letter)
+            cipher_text += key[index]
 
-for letter in plain_text:
-    index = chars.index(letter)
-    cipher_text += key[index]
+    else:
+        cipher_text = TypeError
 
-print(f"Mensaje original :\n{plain_text}")
-print(f"Mensaje encriptado :\n{cipher_text}")
+    return cipher_text
 
-#DECRYPT
-cipher_text = input("Ingrese el mensaje encriptado : ")
-plain_text = ""
+def decrypt(text):
 
-for letter in cipher_text:
-    index = key.index(letter)
-    plain_text += chars[index]
+    if modulo_Mina.test(text) == 'class "str"':
+        chars = " " + string.punctuation + string.digits + string.ascii_letters
+        chars = list(chars)
+        key = chars.copy(); random.shuffle(key)
+        plain_text = ""
+    
+        for letter in text:
+            index = key.index(letter)
+            plain_text += chars[index]
 
-print(f"Mensaje encriptado :\n{cipher_text}")
-print(f"Mensaje original :\n{plain_text}")
+    else:
+        plain_text = TypeError
+
+    return plain_text
